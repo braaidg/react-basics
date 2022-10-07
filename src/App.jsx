e21ac620bd9a9ae813cef31b5ec35ba32180b59b
Braian Gomez <braiandenisgomez@gmail.com>
2022-10-07 16:54:11 -0300

    Clean project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-
-import Index from './pages/Index'
-import Create from './pages/Create'
-import View from './pages/View'
-import Store from "./store/Store"
 
 function App() {
 
   return (
-      <Store>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index/>} />
-            <Route path="create" element={<Create/>} />
-            <Route path="view/:bookId" element={<View/>} />
-          </Routes>
-        </BrowserRouter>
-      </Store>
+    <div>asdasd</div>
   )
 }
 
